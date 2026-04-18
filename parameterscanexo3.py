4ea70a5b60c66d0dbf110b28875eb3d2d05ef39d
Nathan-Imbiky <nimbiky@gmail.com>
2026-04-18 16:35:23 +0200

    Add files via upload

---
+++ parameterscanexo3.py
@@ -0,0 +1,84 @@
+import numpy as np
+import subprocess
+import os
+
+# Parameters
+repertoire = ''
+executable = 'engine.exe'
+input_filename = 'configuration.in.example' # Strictly no longer needed, but we keep it for now to avoid having to change the code in engine.cpp
+
+
+
+input_parameters = {
+    'tf': 2, # t final (overwritten if N >0)
+    'nsteps': 10000, # number of time steps per period (if N>0), number of timesteps total if N=0
+    'G' : 6.674*1e-11, 
+    'mA' :8500, 
+    'd' :5.02, 
+    'r0' :314159*1e3, 
+    'v0' :1.2*1e3, 
+    'h' :10000, 
+    'mT' :5.972e24, 
+    'mL' :7.3477e22*1e-14, 
+    'rho_0' :1.2, 
+    'R_T' :6378.1e3, 
+    'lambda' :7238.2, 
+    'Cx' :0.3,
+    'dTL':384748e3,
+    'rL' :1737.4e3,
+    
+    'x1': 1e-8,
+    'x2':0.0,
+    'x3':,
+    'y1': 1e-8,
+    'y2':0.0,
+    'y3':,
+    'vx1': 1e-8,
+    'vx2':0.0,
+    'vx3':0.0,
+    'vy1': 0.0,
+    'vy2':0.0,
+    'vy3':0.0,
+    'sampling': 1
+}
+
+# -------------------------------------------------
+
+# Updated from last time, the code below can now be used to scan any parameter, just make sure to update the paramstr and the variable_array accordingly
+
+paramstr = 'nsteps' # The parameter to scan, must be one of the keys in input_parameters
+
+variable_array = 2**np.arange(3, 15)  # Example values for the parameter scan
+
+
+outstr = f"pendulum_x1_{input_parameters['x1']:.2g}_y1_{input_parameters['y1']:.2g}_vx1_{input_parameters['x1']:.2g}_vy1_{input_parameters['y1']:.2g}"//////////////////////////////////////
+
+# -------------------------------------------------
+# Create output directory (2 significant digits)
+# -------------------------------------------------
+outdir = f"Scan_{paramstr}_{outstr}"
+os.makedirs(outdir, exist_ok=True)
+print("Saving results in:", outdir)
+
+
+for i in range(len(variable_array)):
+
+    # Copy parameters and overwrite scanned one
+    params = input_parameters.copy()
+    params[paramstr] = variable_array[i]
+
+    output_file = f"{outstr}_{paramstr}_{variable_array[i]}.txt"
+    output_path = os.path.join(outdir, output_file)
+
+    # Build parameter string
+    param_string = " ".join(f"{k}={v:.15g}" for k, v in params.items())
+
+    cmd = (
+        f"{repertoire}{executable} {input_filename} "
+        f"{param_string} output={output_path}"
+    )
+
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+    print("Done.")
+
